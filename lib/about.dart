32374e06b64d392af2e916303e2e06e74ece056a
vipin-v <vipindas233@gmail.com>
2023-06-30 18:11:05 +0530

    removed un-necessary files

--- lib/about.dart
+++ lib/about.dart
@@ -9,6 +9,27 @@ class AboutPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("About Page"),
       ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 10.0,
+          ),
+          Container(
+            width: double.infinity,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 10.0,
+              vertical: 5.0,
+            ),
+            decoration: const BoxDecoration(boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                spreadRadius: 2,
+                blurRadius: 2,
+              )
+            ]),
+          ),
+        ],
+      ),
     );
   }
 }
